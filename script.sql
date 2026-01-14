2d1abfbde5386a64c52cd19c32d811ef5b14440d
viniciosAnhas <viniciosanhas96@gmail.com>
2026-01-14 15:57:02 -0300

    add shutdown e startup

--- script.sql
+++ script.sql
@@ -6,3 +6,9 @@ lsnrctl start
 
 -- Mostra o nome e o estado (ex: OPEN, MOUNTED) da instância do banco de dados atual.
 SELECT instance_name, status FROM v$instance;
+
+-- Desliga o banco de dados imediatamente, desconectando sessões, revertendo transações pendentes e fechando os arquivos de forma limpa.
+shutdown immediate
+
+-- Inicia a instância Oracle, monta o banco de dados e o abre para uso normal.
+startup
