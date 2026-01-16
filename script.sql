ffbc19ab2949dcb1dd8a119a1fb7b4b055fc0900
viniciosAnhas <viniciosanhas96@gmail.com>
2026-01-15 21:14:11 -0300

    add criacao de usuario, add privilegios, add remover privilegios e comando de mudar de PDB

--- script.sql
+++ script.sql
@@ -21,3 +21,24 @@ show con_name;
 
 -- Lista os nomes de todos os PDBs (pluggable databases) existentes dentro do CDB.
 SELECT name FROM v$pdbs;
+
+-- Cria um usuário comum (common user) visível em todos os containers (CDB e todos os PDBs).
+CREATE USER C##DEVOPS identified by "dev123" container=all;
+
+-- Concede o privilégio de conexão ao usuário C##DEVOPS em todos os containers.
+GRANT connect to C##DEVOPS container=all;
+
+-- Concede permissão para criar tabelas apenas no container atual (CDB ou PDB) onde o comando foi executado.
+GRANT CREATE TABLE to C##DEVOPS container=current;
+
+-- Exibe o nome do usuário com o qual a sessão atual está conectada ao banco de dados.
+show user;
+
+-- Conecta (ou tenta conectar) ao banco de dados usando o usuário C##DEVOPS e a senha dev123 na instância/container atual.
+conn C##DEVOPS/dev123;
+
+-- Remove a permissão de criar tabelas do usuário C##DEVOPS apenas no container atual (CDB ou PDB onde o comando é executado).
+REVOKE CREATE TABLE FROM C##DEVOPS container=current;
+
+-- Muda o contexto da sessão atual para o PDB chamado "ORCLPDB", permitindo trabalhar dentro desse banco de dados plugável específico.
+ALTER SESSION SET container=ORCLPDB;
